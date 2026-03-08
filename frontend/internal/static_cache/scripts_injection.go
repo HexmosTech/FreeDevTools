42b6f0c2dac52a0e5bd7b9a386c3c11cd57fcaa2
Lince Mathew <linz07m@gmail.com>
2026-03-08 15:46:49 +0530

    remove unwanted files
    LiveReview Pre-Commit Check: vouched (iter:1, coverage:0%)

--- frontend/internal/static_cache/scripts_injection.go
+++
@@ -1,7 +0,0 @@
-package static_cache
-
-// InjectScripts no longer needs to inject JS strings since they are served statically
-// It simply returns the HTML untouched.
-func InjectScripts(html []byte) []byte {
-	return html
-}
